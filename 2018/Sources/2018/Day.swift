210742aa8d46c9f9997b4de3bff15b0550989b1b
Jais Cheema <jaischeema@gmail.com>
2018-12-09 18:56:52 +1100

    Organise the solutions in better structure

--- 2018/Sources/2018/Day.swift
+++ 2018/Sources/2018/Day.swift
@@ -6,3 +6,13 @@ protocol Day {
 
     var day: Int { get }
 }
+
+// Example solution
+//
+// class DayN: Day {
+//     var day: Int { return N }
+//     required init() {}
+//
+//     func puzzle1() {}
+//     func puzzle2() {}
+// }
